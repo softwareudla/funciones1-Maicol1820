73bdbb4b712e39d9fdff27a7e50de4736616fb7a
Maicol1820 <133879773+Maicol1820@users.noreply.github.com>
2024-10-22 14:38:49 -0500

    Update funciones.h

---
+++ funciones.h
@@ -0,0 +1,6 @@
+void LeerNombreProductos(char productos[10][30], float precios[10], int cantidad);
+float CalcularValorTotal(float precios[10], int cantidad);
+int ProductoMasCaro(float precios[10], int cantidad);
+int ProductoMasBarato(float precios[10], int cantidad);
+float CalcularPromedio(float precios[10], int cantidad);
+int BuscarProducto(char productos[10][30], char nombre[30], int cantidad);
